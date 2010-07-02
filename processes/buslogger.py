87566e96375c3afd3064afe29829427a74eb17f4
|ALPHA| Mad Professor <alpha.mad.professor@gmail.com>
2010-07-02 01:57:20 -0400

    Use logging module.

--- processes/buslogger.py
+++ processes/buslogger.py
@@ -1,17 +1,23 @@
 # dspytbus.py - the Desperately Silly PYthon Threaded message BUS
 # See files README and COPYING for copyright and licensing details.
 
+import logging
 from dspytbus import Component
-from datetime import datetime
+
+LOGGER = logging.getLogger('dspytbus.BusLogger')
 
 class BusLogger(Component):
     """
     Simple bus logger.
 
-    This will simply print all the messages going across the bus.
+    Log all messages of interest across the bus.
     """
     def handle_message(self, message):
-        print datetime.now(), "=====", message, "====="
+        LOGGER.info(message)
 
 if __name__ == "__main__":
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(levelname)s - %(message)s"
+    )
     BusLogger().start()
